58674d6b6c423430e6baa105263e0af4f6e4b7a0
Jonathan Butler <xxbutler86xx@gmail.com>
2024-05-21 19:39:34 -0400

    Fade picture and text correctly.

---
+++ js/index.js
@@ -0,0 +1,38 @@
+
+
+// $(".top-nav-bar").addClass("hidden");
+
+// $(".top-nav-bar").hide().slideDown(500);
+
+$(".jon-head-shot-hi").addClass("hidden");
+// $(".hi-jonathan").addClass("hidden");
+$(".cybersecurity-professional").addClass("hidden");
+
+
+setTimeout(() => {
+    $(".jon-head-shot-hi").removeClass("hidden");
+    $(".jon-head-shot-hi").hide().fadeIn(750);
+}, 1000)
+
+// setTimeout(() => {
+//     $(".hi-jonathan").removeClass("hidden");
+//     $(".hi-jonathan").hide().fadeIn(750);
+// }, 1500)
+
+setTimeout(() => {
+    $(".cybersecurity-professional").removeClass("hidden");
+    $(".cybersecurity-professional").hide().fadeIn(750);
+}, 2500)
+
+
+
+// $(".headshot-div").removeClass("hidden");
+
+
+// $(".hi-jonathan").fadeIn();
+
+
+
+// $(".bottom-nav-bar").hide().slideDown();
+
+
