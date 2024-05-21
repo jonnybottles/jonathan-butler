2651ed5755c71ae0b7048f8ce7ad1decb82cdfe3
Jonathan Butler <xxbutler86xx@gmail.com>
2024-05-21 19:59:11 -0400

    Modify so top bar slides down and doesnt move other elements.

--- js/index.js
+++ js/index.js
@@ -1,38 +1,19 @@
+$(document).ready(function() {
+    $(".jon-head-shot-hi").addClass("hidden");
+    $(".cybersecurity-professional").addClass("hidden");
 
+    setTimeout(() => {
+        $(".jon-head-shot-hi").removeClass("hidden");
+        $(".jon-head-shot-hi").hide().fadeIn(750);
+    }, 1000);
 
-// $(".top-nav-bar").addClass("hidden");
+    setTimeout(() => {
+        $(".cybersecurity-professional").removeClass("hidden");
+        $(".cybersecurity-professional").hide().fadeIn(750);
+    }, 2500);
 
-// $(".top-nav-bar").hide().slideDown(500);
-
-$(".jon-head-shot-hi").addClass("hidden");
-// $(".hi-jonathan").addClass("hidden");
-$(".cybersecurity-professional").addClass("hidden");
-
-
-setTimeout(() => {
-    $(".jon-head-shot-hi").removeClass("hidden");
-    $(".jon-head-shot-hi").hide().fadeIn(750);
-}, 1000)
-
-// setTimeout(() => {
-//     $(".hi-jonathan").removeClass("hidden");
-//     $(".hi-jonathan").hide().fadeIn(750);
-// }, 1500)
-
-setTimeout(() => {
-    $(".cybersecurity-professional").removeClass("hidden");
-    $(".cybersecurity-professional").hide().fadeIn(750);
-}, 2500)
-
-
-
-// $(".headshot-div").removeClass("hidden");
-
-
-// $(".hi-jonathan").fadeIn();
-
-
-
-// $(".bottom-nav-bar").hide().slideDown();
-
-
+    setTimeout(() => {
+        $(".top-nav-bar").removeClass("hidden");
+        $(".top-nav-bar").hide().slideDown(750);
+    }, 3500);
+});
