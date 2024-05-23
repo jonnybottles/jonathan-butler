120e5ec63634894ad61f2691606926c42fcb24c4
Jonathan Butler <xxbutler86xx@gmail.com>
2024-05-23 14:45:10 -0400

    Header drops down smoothly, footer needs to slide up appropraitely.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,20 @@
-$(document).ready(function() {
-    $(".jon-head-shot-hi").addClass("hidden");
-    $(".cybersecurity-professional").addClass("hidden");
+$(".jon-head-shot-hi").addClass("hidden");
+$(".cybersecurity-professional").addClass("hidden");
+$(".footer-container").addClass("hidden");
 
-    setTimeout(() => {
-        $(".jon-head-shot-hi").removeClass("hidden");
-        $(".jon-head-shot-hi").hide().fadeIn(750);
-    }, 1000);
+setTimeout(() => {
+    $(".jon-head-shot-hi").removeClass("hidden");
+    $(".jon-head-shot-hi").hide().fadeIn(750);
+}, 1000);
 
-    setTimeout(() => {
-        $(".cybersecurity-professional").removeClass("hidden");
-        $(".cybersecurity-professional").hide().fadeIn(750);
-    }, 2500);
+setTimeout(() => {
+    $(".cybersecurity-professional").removeClass("hidden");
+    $(".cybersecurity-professional").hide().fadeIn(750);
+}, 2500);
 
-    setTimeout(() => {
-        $(".top-nav-bar").removeClass("hidden");
-        $(".top-nav-bar").hide().slideDown(750);
-    }, 3500);
-});
+setTimeout(() => {
+    $(".top-nav-bar").removeClass("hidden");
+    $(".top-nav-bar").css("top", "-100px"); // Ensure initial position is off-screen
+    $(".top-nav-bar").animate({ top: "0px" }, 750); // Slide down smoothly
+
+}, 3500);
