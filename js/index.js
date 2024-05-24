acb2a7b906965443137dca1887233c20b70456c6
Jonathan Butler <xxbutler86xx@gmail.com>
2024-05-24 11:07:13 -0400

    Add carsoul and center.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
+
+
 $(".jon-head-shot-hi").addClass("hidden");
 $(".cybersecurity-professional").addClass("hidden");
-// $(".footer-container").addClass("hidden");
 
 setTimeout(() => {
     $(".jon-head-shot-hi").removeClass("hidden");
+    $(".jon-head-shot-hi").css("visibility", "visible");
     $(".jon-head-shot-hi").hide().fadeIn(750);
 }, 1000);
 
 setTimeout(() => {
     $(".cybersecurity-professional").removeClass("hidden");
+    $(".cybersecurity-professional").css("visibility", "visible");
     $(".cybersecurity-professional").hide().fadeIn(750);
 }, 2500);
 
