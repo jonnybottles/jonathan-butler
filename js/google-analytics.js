f49115aaf1ed3993c9f33d46f6bc2f626bf4a48b
Jonathan Butler <xxbutler86xx@gmail.com>
2025-01-31 11:22:22 -0500

    Add google analytics capability.

---
+++ js/google-analytics.js
@@ -0,0 +1,7 @@
+// analytics.js
+(function() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-SDXMT5P8V5');
+})();
